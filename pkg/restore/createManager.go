33072674f9ea9cfc9e18981068c4be22e7aabdd1
Jeremias Weber <jeremias.weber@cloudogu.com>
2023-10-18 15:17:15 +0200

    #7 restore without getting the backup CR
    This is necessary because the backup CR might not exist.

--- pkg/restore/createManager.go
+++ pkg/restore/createManager.go
@@ -4,15 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/cloudogu/k8s-backup-operator/pkg/maintenance"
-	restoreprovider "github.com/cloudogu/k8s-backup-operator/pkg/provider"
-	"github.com/cloudogu/k8s-backup-operator/pkg/requeue"
+
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	v1 "github.com/cloudogu/k8s-backup-operator/pkg/api/v1"
+	"github.com/cloudogu/k8s-backup-operator/pkg/maintenance"
+	restoreprovider "github.com/cloudogu/k8s-backup-operator/pkg/provider"
 )
 
 const (
@@ -22,14 +20,13 @@ const (
 
 type defaultCreateManager struct {
 	restoreClient         ecosystemRestoreInterface
-	backupClient          ecosystemBackupInterface
 	recorder              eventRecorder
 	maintenanceModeSwitch maintenanceModeSwitch
 }
 
-func newCreateManager(restoreClient ecosystemRestoreInterface, backupClient ecosystemBackupInterface, recorder eventRecorder, registry cesRegistry) *defaultCreateManager {
+func newCreateManager(restoreClient ecosystemRestoreInterface, recorder eventRecorder, registry cesRegistry) *defaultCreateManager {
 	maintenanceSwitch := maintenance.New(registry.GlobalConfig())
-	return &defaultCreateManager{restoreClient: restoreClient, backupClient: backupClient, recorder: recorder, maintenanceModeSwitch: maintenanceSwitch}
+	return &defaultCreateManager{restoreClient: restoreClient, recorder: recorder, maintenanceModeSwitch: maintenanceSwitch}
 }
 
 func (cm *defaultCreateManager) create(ctx context.Context, restore *v1.Restore) error {
@@ -47,14 +44,9 @@ func (cm *defaultCreateManager) create(ctx context.Context, restore *v1.Restore)
 		return fmt.Errorf("failed to add finalizer [%s] in restore resource [%s]: %w", v1.RestoreFinalizer, restoreName, err)
 	}
 
-	backup, err := cm.getBackupFromRestore(ctx, restore)
+	provider, err := restoreprovider.GetProvider(ctx, restore.Spec.Provider, restore.Namespace, cm.recorder)
 	if err != nil {
-		return err
-	}
-
-	provider, err := restoreprovider.GetProvider(ctx, backup.Spec.Provider, restore.Namespace, cm.recorder)
-	if err != nil {
-		return fmt.Errorf("failed to get restore provider [%s]: %w", backup.Spec.Provider, err)
+		return fmt.Errorf("failed to get restore provider [%s]: %w", restore.Spec.Provider, err)
 	}
 
 	err = cm.maintenanceModeSwitch.ActivateMaintenanceMode(maintenanceModeTitle, maintenanceModeText)
@@ -86,21 +78,4 @@ func (cm *defaultCreateManager) create(ctx context.Context, restore *v1.Restore)
 	}
 
 	return nil
-}
-
-func (cm *defaultCreateManager) getBackupFromRestore(ctx context.Context, restore *v1.Restore) (*v1.Backup, error) {
-	backupName := restore.Spec.BackupName
-	backup, err := cm.backupClient.Get(ctx, backupName, metav1.GetOptions{})
-	if err != nil {
-		if apierrors.IsNotFound(err) {
-			return nil, fmt.Errorf("found no backup with name [%s] for restore resource [%s]: %w", backupName, restore.Name, err)
-		}
-
-		return nil, &requeue.GenericRequeueableError{
-			ErrMsg: fmt.Sprintf("failed to get backup [%s] for restore resource [%s]", backupName, restore.Name),
-			Err:    err,
-		}
-	}
-
-	return backup, nil
 }
