20d76b838a21ab950f28f73002bda9c5174e6a48
meiserloh <matthias.eiserloh@cloudogu.com>
2023-10-27 14:35:17 +0200

    #8 add delete cron job function
    Co-authored-by: Jeremias Weber <jeremias.weber@cloudogu.com>

--- pkg/backupschedule/deleteManager.go
+++ pkg/backupschedule/deleteManager.go
@@ -2,6 +2,9 @@ package backupschedule
 
 import (
 	"context"
+	"fmt"
+	"github.com/cloudogu/k8s-backup-operator/pkg/retry"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1 "github.com/cloudogu/k8s-backup-operator/pkg/api/v1"
 )
@@ -17,6 +20,23 @@ func newDeleteManager(clientSet ecosystemInterface, recorder eventRecorder, name
 }
 
 func (dm *defaultDeleteManager) delete(ctx context.Context, backupSchedule *v1.BackupSchedule) error {
-	//TODO implement me
-	panic("implement me")
+	schedulesClient := dm.clientSet.EcosystemV1Alpha1().BackupSchedules(dm.namespace)
+	_, err := schedulesClient.UpdateStatusDeleting(ctx, backupSchedule)
+	if err != nil {
+		return fmt.Errorf("failed to set status [%s] in backup schedule resource [%s]: %w", v1.BackupScheduleStatusDeleting, backupSchedule.Name, err)
+	}
+
+	err = retry.OnError(5, retry.AlwaysRetryFunc, func() error {
+		return dm.clientSet.BatchV1().CronJobs(dm.namespace).Delete(ctx, backupSchedule.CronJobName(), metav1.DeleteOptions{})
+	})
+	if err != nil {
+		return fmt.Errorf("failed to delete CronJob %s: %w", backupSchedule.CronJobName(), err)
+	}
+
+	_, err = schedulesClient.RemoveFinalizer(ctx, backupSchedule, v1.BackupScheduleFinalizer)
+	if err != nil {
+		return fmt.Errorf("failed to remove finalizer [%s] in backup schedule resource [%s]: %w", v1.BackupScheduleFinalizer, backupSchedule.Name, err)
+	}
+
+	return nil
 }
