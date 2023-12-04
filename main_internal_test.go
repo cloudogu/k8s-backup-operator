82eb31757f87431e2477af3858f810f10b62cd09
Jeremias Weber <jeremias.weber@cloudogu.com>
2023-12-04 13:34:37 +0100

    #17 add tests that have gone missing

--- main_internal_test.go
+++ main_internal_test.go
@@ -5,7 +5,9 @@ import (
 	"flag"
 	"github.com/cloudogu/k8s-backup-operator/pkg/additionalimages"
 	"github.com/cloudogu/k8s-backup-operator/pkg/api/ecosystem"
+	"github.com/cloudogu/k8s-backup-operator/pkg/garbagecollection"
 	"github.com/cloudogu/k8s-backup-operator/pkg/provider"
+	"github.com/cloudogu/k8s-backup-operator/pkg/scheduledbackup"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/record"
 	"testing"
@@ -594,6 +596,138 @@ func Test_startOperator(t *testing.T) {
 
 		// when
 		err := startOperator(testCtx, flags, []string{})
+
+		// then
+		require.NoError(t, err)
+	})
+}
+
+func Test_startGarbageCollector(t *testing.T) {
+	t.Run("should fail to schedule backup", func(t *testing.T) {
+		// given
+		t.Setenv("NAMESPACE", "ecosystem")
+		t.Setenv("STAGE", "development")
+
+		oldGetConfigFunc := ctrl.GetConfigOrDie
+		oldNewGarbageCollectionManagerFunc := newGarbageCollectionManager
+		defer func() {
+			ctrl.GetConfigOrDie = oldGetConfigFunc
+			newGarbageCollectionManager = oldNewGarbageCollectionManagerFunc
+		}()
+
+		restConfig := &rest.Config{}
+		ctrl.GetConfigOrDie = func() *rest.Config {
+			return restConfig
+		}
+
+		gcManagerMock := newMockGcManager(t)
+		gcManagerMock.EXPECT().CollectGarbage(testCtx).Return(assert.AnError)
+		newGarbageCollectionManager = func(clientSet ecosystem.Interface, namespace string, strategyName string) garbagecollection.Manager {
+			return gcManagerMock
+		}
+
+		flags := flag.NewFlagSet("gc", flag.ContinueOnError)
+
+		// when
+		err := startGarbageCollector(testCtx, flags, []string{})
+
+		// then
+		require.Error(t, err)
+		assert.ErrorIs(t, err, assert.AnError)
+	})
+	t.Run("should succeed to schedule backup", func(t *testing.T) {
+		// given
+		t.Setenv("NAMESPACE", "ecosystem")
+		t.Setenv("STAGE", "development")
+
+		oldGetConfigFunc := ctrl.GetConfigOrDie
+		oldNewGarbageCollectionManagerFunc := newGarbageCollectionManager
+		defer func() {
+			ctrl.GetConfigOrDie = oldGetConfigFunc
+			newGarbageCollectionManager = oldNewGarbageCollectionManagerFunc
+		}()
+
+		restConfig := &rest.Config{}
+		ctrl.GetConfigOrDie = func() *rest.Config {
+			return restConfig
+		}
+
+		gcManagerMock := newMockGcManager(t)
+		gcManagerMock.EXPECT().CollectGarbage(testCtx).Return(nil)
+		newGarbageCollectionManager = func(clientSet ecosystem.Interface, namespace string, strategyName string) garbagecollection.Manager {
+			return gcManagerMock
+		}
+
+		flags := flag.NewFlagSet("gc", flag.ContinueOnError)
+
+		// when
+		err := startGarbageCollector(testCtx, flags, []string{})
+
+		// then
+		require.NoError(t, err)
+	})
+}
+
+func Test_startScheduledBackup(t *testing.T) {
+	t.Run("should fail to schedule backup", func(t *testing.T) {
+		// given
+		t.Setenv("NAMESPACE", "ecosystem")
+		t.Setenv("STAGE", "development")
+
+		oldGetConfigFunc := ctrl.GetConfigOrDie
+		oldNewScheduledBackupManagerFunc := newScheduledBackupManager
+		defer func() {
+			ctrl.GetConfigOrDie = oldGetConfigFunc
+			newScheduledBackupManager = oldNewScheduledBackupManagerFunc
+		}()
+
+		restConfig := &rest.Config{}
+		ctrl.GetConfigOrDie = func() *rest.Config {
+			return restConfig
+		}
+
+		scheduledBackupManagerMock := newMockScheduledBackupManager(t)
+		scheduledBackupManagerMock.EXPECT().ScheduleBackup(testCtx).Return(assert.AnError)
+		newScheduledBackupManager = func(clientSet ecosystem.Interface, options scheduledbackup.Options) scheduledbackup.Manager {
+			return scheduledBackupManagerMock
+		}
+
+		flags := flag.NewFlagSet("scheduled-backup", flag.ContinueOnError)
+
+		// when
+		err := startScheduledBackup(testCtx, flags, []string{"--name=banana", "--provider=velero"})
+
+		// then
+		require.Error(t, err)
+		assert.ErrorIs(t, err, assert.AnError)
+	})
+	t.Run("should succeed to schedule backup", func(t *testing.T) {
+		// given
+		t.Setenv("NAMESPACE", "ecosystem")
+		t.Setenv("STAGE", "development")
+
+		oldGetConfigFunc := ctrl.GetConfigOrDie
+		oldNewScheduledBackupManagerFunc := newScheduledBackupManager
+		defer func() {
+			ctrl.GetConfigOrDie = oldGetConfigFunc
+			newScheduledBackupManager = oldNewScheduledBackupManagerFunc
+		}()
+
+		restConfig := &rest.Config{}
+		ctrl.GetConfigOrDie = func() *rest.Config {
+			return restConfig
+		}
+
+		scheduledBackupManagerMock := newMockScheduledBackupManager(t)
+		scheduledBackupManagerMock.EXPECT().ScheduleBackup(testCtx).Return(nil)
+		newScheduledBackupManager = func(clientSet ecosystem.Interface, options scheduledbackup.Options) scheduledbackup.Manager {
+			return scheduledBackupManagerMock
+		}
+
+		flags := flag.NewFlagSet("scheduled-backup", flag.ContinueOnError)
+
+		// when
+		err := startScheduledBackup(testCtx, flags, []string{"--name=banana", "--provider=velero"})
 
 		// then
 		require.NoError(t, err)
