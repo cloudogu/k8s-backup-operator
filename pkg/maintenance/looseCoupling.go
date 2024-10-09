135ca281ab28f914f4f97f49d0d6140c11f58516
Niklas <niklas.hinze@cloudogu.com>
2024-10-09 07:40:05 +0200

    #31 delete remaining etcd occurrences

--- pkg/maintenance/looseCoupling.go
+++ pkg/maintenance/looseCoupling.go
@@ -2,16 +2,13 @@ package maintenance
 
 import (
 	"context"
-	"time"
 )
-
-var waitForEtcdTimeout = 5 * time.Minute
 
 type looselyCoupledMaintenanceSwitch struct {
 	maintenanceModeSwitch
 }
 
-// NewWithLooseCoupling creates a switch that checks if the configuration registry (e.g., etcd) exists before switching.
+// NewWithLooseCoupling creates a switch that checks if the configuration registry exists before switching.
 // If the registry does not exist, no switch is executed.
 func NewWithLooseCoupling(globalConfigRepository globalConfigRepository) *looselyCoupledMaintenanceSwitch {
 	return &looselyCoupledMaintenanceSwitch{
@@ -19,14 +16,14 @@ func NewWithLooseCoupling(globalConfigRepository globalConfigRepository) *loosel
 	}
 }
 
-// ActivateMaintenanceMode activates the maintenance mode if the etcd exists and is ready.
+// ActivateMaintenanceMode activates the maintenance mode if the global registry exists and is ready.
 // This loose coupling enables us to perform restores on an empty cluster.
 func (lcms *looselyCoupledMaintenanceSwitch) ActivateMaintenanceMode(ctx context.Context, title string, text string) error {
 	return lcms.maintenanceModeSwitch.ActivateMaintenanceMode(ctx, title, text)
 }
 
-// DeactivateMaintenanceMode waits until the etcd is ready and then deactivates the maintenance mode.
-// While this is not directly loose coupling, we trust that an instance of etcd will be restored.
+// DeactivateMaintenanceMode waits until the global registry is ready and then deactivates the maintenance mode.
+// While this is not directly loose coupling, we trust that an instance of the global registry will be restored.
 func (lcms *looselyCoupledMaintenanceSwitch) DeactivateMaintenanceMode(ctx context.Context) error {
 	return lcms.maintenanceModeSwitch.DeactivateMaintenanceMode(ctx)
 }
