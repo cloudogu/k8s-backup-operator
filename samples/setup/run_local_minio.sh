e6947a604f4132aafba9a266dfddb3f040f842a6
Niklas Roeske <niklas.roeske@cloudogu.com>
2025-07-11 12:52:45 +0200

    #67 update run_local_minio.sh to create access keys and buckets
    The minio image was changed to the community edition. It's not possible to create access keys with the UI

--- samples/setup/run_local_minio.sh
+++ samples/setup/run_local_minio.sh
@@ -4,3 +4,10 @@ set -o nounset
 set -o pipefail
 
 docker run -d --name minio -p 9000:9000 -p 9090:9090 -e "MINIO_ROOT_USER=admin123" -e "MINIO_ROOT_PASSWORD=admin123" quay.io/minio/minio server /data --console-address ":9090"
+
+
+docker exec -t minio mc alias set minio http://localhost:9000 admin123 admin123
+docker exec -t minio mc admin accesskey create minio/ --access-key MY-ACCESS-KEY --secret-key MY-ACCESS-SECRET123
+docker exec -t minio mc admin accesskey create minio/ --access-key MY-VELERO-ACCESS-KEY --secret-key MY-VELERO.ACCESS-SECRET123
+docker exec -t minio mc mb minio/velero --region minio-default
+docker exec -t minio mc mb minio/longhorn --region minio-default
