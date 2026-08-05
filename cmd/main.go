dcabd807ddc390772e9d6adb54ee11f62a710116
rebecca-freiburg <rebecca.freiburg-extern@cloudogu.com>
2026-08-05 13:56:34 +0200

    [#125] fix issues after wrong merge conflict resolution

--- cmd/main.go
+++ cmd/main.go
@@ -8,13 +8,14 @@ import (
 	"os"
 	"time"
 
+	backup2 "github.com/cloudogu/k8s-backup-operator/internal/controller/backup"
 	restorecontroller "github.com/cloudogu/k8s-backup-operator/internal/controller/restore"
-	schedulecontroller "github.com/cloudogu/k8s-backup-operator/internal/controller/schedule"
 	"github.com/cloudogu/k8s-backup-operator/pkg/metrics"
 	"github.com/cloudogu/k8s-backup-operator/pkg/provider"
 	"github.com/cloudogu/k8s-backup-operator/pkg/scale"
-	blueprintv3 "github.com/cloudogu/k8s-blueprint-lib/v3/client"
+	blueprintv3 "github.com/cloudogu/k8s-blueprint-lib/v3/api/v3"
 	doguv2Client "github.com/cloudogu/k8s-dogu-lib/v2/client"
+	"github.com/cloudogu/k8s-registry-lib/repository"
 	velerov1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
 	"k8s.io/client-go/dynamic"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -36,7 +37,7 @@ import (
 	"github.com/cloudogu/k8s-backup-lib/api/ecosystem"
 	k8sv1 "github.com/cloudogu/k8s-backup-lib/api/v1"
 	"github.com/cloudogu/k8s-backup-operator/pkg/additionalimages"
-	"github.com/cloudogu/k8s-backup-operator/pkg/backup"
+	"github.com/cloudogu/k8s-backup-operator/pkg/backupschedule"
 	"github.com/cloudogu/k8s-backup-operator/pkg/cleanup"
 	"github.com/cloudogu/k8s-backup-operator/pkg/config"
 	"github.com/cloudogu/k8s-backup-operator/pkg/garbagecollection"
@@ -77,6 +78,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 	utilruntime.Must(k8sv1.AddToScheme(scheme))
 	utilruntime.Must(velerov1.AddToScheme(scheme))
+	utilruntime.Must(blueprintv3.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -283,13 +285,6 @@ func configureReconcilers(ctx context.Context, k8sManager controllerManager, ope
 		return fmt.Errorf("unable to create ecosystem clientset: %w", err)
 	}
 
-	blueprintClientSet, err := blueprintv3.NewClientSet(k8sManager.GetConfig(), k8sClientSet)
-	if err != nil {
-		return fmt.Errorf("unable to create blueprint clientset: %w", err)
-	}
-
-	blueprintClient := blueprintClientSet.EcosystemV1Alpha1().Blueprints(operatorConfig.Namespace)
-
 	doguClient, err := doguv2Client.NewForConfig(k8sManager.GetConfig())
 	if err != nil {
 		return fmt.Errorf("unable to create dogu client: %w", err)
@@ -343,15 +338,29 @@ func configureReconcilers(ctx context.Context, k8sManager controllerManager, ope
 		return fmt.Errorf("unable to create restore controller: %w", err)
 	}
 
-	backupManager := backup.NewBackupManager(k8sClient, ecosystemClientSet, blueprintClient, operatorConfig.Namespace, recorder, configGetter)
-	if err = (backup.NewBackupReconciler(ecosystemClientSet, recorder, operatorConfig.Namespace, backupManager, requeueHandler, configGetter)).SetupWithManager(k8sManager); err != nil {
-		return fmt.Errorf("unable to create backup controller: %w", err)
+	err = configureBackupReconcilers(k8sManager, operatorConfig.Namespace)
+	if err != nil {
+		return fmt.Errorf("error setting up backup controller with manager: %w", err)
 	}
 
-	if err = schedulecontroller.NewController(k8sClient, operatorImage, operatorConfig.ImagePullSecrets).SetupWithManager(k8sManager); err != nil {
+	if err = backupschedule.NewReconciler(ecosystemClientSet, recorder, operatorConfig.Namespace, requeueHandler, imageConfig, operatorConfig.ImagePullSecrets).SetupWithManager(k8sManager); err != nil {
 		return fmt.Errorf("unable to create backupSchedule controller: %w", err)
 	}
 	// +kubebuilder:scaffold:builder
+
+	return nil
+}
+
+func configureBackupReconcilers(k8sManager controllerManager, namespace string) error {
+	k8sClient := k8sManager.GetClient()
+	maintenanceModeAdapter := repository.NewMaintenanceModeAdapter("k8s-backup-operator", k8sClient, namespace)
+	maintenanceGateway := backup2.NewMaintenanceGateway(maintenanceModeAdapter)
+	backupReconciler := backup2.NewReconciler(k8sClient, maintenanceGateway, backup2.DefaultClock{})
+	backupController := backup2.NewController(k8sClient, backupReconciler)
+	err := backupController.SetupWithManager(k8sManager)
+	if err != nil {
+		return fmt.Errorf("error setting up backup controller with manager: %w", err)
+	}
 
 	return nil
 }
